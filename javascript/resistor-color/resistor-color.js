12d14bf0279ebb54334fcace2f74885dfa0be16b
valmar parts <valmarparts@gmail.com>
2021-04-03 01:20:28 +0300

    completed resistor colors

---
+++ javascript/resistor-color/resistor-color.js
@@ -0,0 +1,23 @@
+//
+// This is only a SKELETON file for the 'Resistor Color' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+//used indexOF
+
+export const colorCode = (colorNumber) => {
+  return COLORS.indexOf(colorNumber);
+};
+
+export const COLORS = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white",
+];
