691d7b03e9beae050eb8e66970cf991d7fa36ad9
valmar parts <valmarparts@gmail.com>
2021-04-06 16:48:06 +0300

    fixed variable name and scientific notation

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -3,9 +3,9 @@
 // convenience to get you started writing code faster.
 //
 
-export const gigasecond = (gsTime) => {
-  var gigaSec = 10 ** 12;
-  var newTime = gsTime.getTime();
+export const gigasecond = (date) => {
+  var gigaSec = 1e12;
+  var newTime = date.getTime();
   var endTime = new Date(newTime + gigaSec);
   return endTime;
 };
